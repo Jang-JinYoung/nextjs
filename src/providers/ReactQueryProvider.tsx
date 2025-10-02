bf02f2ccdd35f146c0d0511538d53f8c676257ea
jyjang <jinyoung_8965@naver.com>
2025-10-02 15:41:26 +0900

    feat : react-qeury, zustand 추가

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,21 @@
+// src/providers/ReactQueryProvider.tsx
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode, useState } from "react";
+
+interface Props {
+    children: ReactNode;
+}
+
+export const ReactQueryProvider: React.FC<Props> = ({ children }) => {
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+};
