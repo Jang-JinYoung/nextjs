798d1c32f31411d0fab1a05c4512cd583029f54e
jyjang <jinyoung_8965@naver.com>
2025-12-10 16:03:35 +0900

    feat: google-map 추가

--- src/app/schedule/components/mapPanel.tsx
+++ src/app/schedule/components/mapPanel.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import styles from "./mapPanel.module.css";
 
 export default function MapPanel() {
+  const center = { lat: 37.4979, lng: 127.0276 };
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.placeholder}>지도 영역</div>
+      <LoadScript googleMapsApiKey={process.env.GOOGLE_MAP_KEY!}>
+        <GoogleMap
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          center={center}
+          zoom={15}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 }
